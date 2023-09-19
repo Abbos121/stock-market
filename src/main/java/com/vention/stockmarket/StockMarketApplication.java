9d5ff65fdcceefedbdde9d6db7c04a62a2558043
Abbos Akramov <abbosakramov121@gmail.com>
2023-09-19 12:35:47 +0500

    initial creational commits

--- src/main/java/com/vention/stockmarket/StockMarketApplication.java
+++ src/main/java/com/vention/stockmarket/StockMarketApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StockMarketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StockMarketApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StockMarketApplication.class, args);
+    }
 
 }
--- src/test/java/com/vention/stockmarket/StockMarketApplicationTests.java
+++ src/test/java/com/vention/stockmarket/StockMarketApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StockMarketApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
