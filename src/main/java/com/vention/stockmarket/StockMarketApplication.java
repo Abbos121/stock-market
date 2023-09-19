64dfe57de853d5458e1c2671e60943bcbcb48d23
Abbos Akramov <abbosakramov121@gmail.com>
2023-09-19 12:30:20 +0500

    Initial commit

---
+++ src/main/java/com/vention/stockmarket/StockMarketApplication.java
@@ -0,0 +1,13 @@
+package com.vention.stockmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketApplication.class, args);
+	}
+
+}
